c98d61274b1e0b853c714b2931d6400e0683e921
Billy Huynh <billyhuynh@live.com>
2019-09-01 21:16:11 +1000

    Updates

---
+++ precache-manifest.c5b3ed4f9d532710a8d3e9cca9380923.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33365776e06ff5e75f1b1b7205c24bfd",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "bc8c25180b1f2d2d3c73",
+    "url": "/tetris/static/css/main.ef0edd1f.chunk.css"
+  },
+  {
+    "revision": "dd578e3185238863f8ec",
+    "url": "/tetris/static/js/2.f67b9e5a.chunk.js"
+  },
+  {
+    "revision": "bc8c25180b1f2d2d3c73",
+    "url": "/tetris/static/js/main.add91248.chunk.js"
+  },
+  {
+    "revision": "476477927e2af76d5e8d",
+    "url": "/tetris/static/js/runtime~main.d4888cca.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "91ccc7ddf053af764047a275b9028b2b",
+    "url": "/tetris/static/media/bg.91ccc7dd.jpg"
+  }
+]);
